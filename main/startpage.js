79cedbee26d4f1fa419ad8348e7688fccc45d145
Paula642 <paula.kropfinger@gmail.com>
2024-04-04 15:51:59 +0200

    function generateLink added

--- main/startpage.js
+++ main/startpage.js
@@ -10,5 +10,12 @@ function changeBackgroundColor(id, value ) {
     button.style.backgroundColor = "green";
     //coloredTheme = id;
     colored[value] = id;
+    if (colored[1] !== "" && colored[2] !== "" && colored[3] !== ""){
+        generateLink()
+    }
 
+}
+
+function generateLink() {
+    let link = "?" + "theme=" + colored[1] + "player=" + colored[2] + "level=" + colored[3];
 }
