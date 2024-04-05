12e39fb2dded71af82575483861f92b0608f5dbb
SafaeKa <safae.kartite@nttdata.com>
2024-04-06 00:26:47 +0200

    Start button is disabled unless all elements are selected + start button directs to index.html

--- main/startpage.js
+++ main/startpage.js
@@ -1,21 +1,31 @@
-let colored = [];
+let colored = {
+    1: "",
+    2: "",
+    3: ""
+};
 
-
-function changeBackgroundColor(id, value ) {
-    if (colored[value]){
-        const buttonColored =  document.getElementById(colored[value]);
+function changeBackgroundColor(id, value) {
+    if (colored[value]) {
+        const buttonColored = document.getElementById(colored[value]);
         buttonColored.style.backgroundColor = "white";
     }
     const button = document.getElementById(id);
     button.style.backgroundColor = "green";
-    //coloredTheme = id;
     colored[value] = id;
-    if (colored[1] !== "" && colored[2] !== "" && colored[3] !== ""){
-        generateLink()
-    }
+    checkOptionsSelected();
+}
 
+function checkOptionsSelected() {
+    const startButton = document.getElementById("startButton");
+    if (colored[1] !== "" && colored[2] !== "" && colored[3] !== "") {
+        startButton.disabled = false;
+    } else {
+        startButton.disabled = true;
+    }
 }
 
 function generateLink() {
-    let link = "?" + "theme=" + colored[1] + "player=" + colored[2] + "level=" + colored[3];
+    let link = "?theme=" + colored[1] + "&player=" + colored[2] + "&level=" + colored[3];
+    window.location.href = "index.html" + link;
 }
+
