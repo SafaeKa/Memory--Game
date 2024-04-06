87b29cbb4d35c96bbe9527c41c05e23e96d2dea4
Paula642 <paula.kropfinger@gmail.com>
2024-04-06 16:31:01 +0200

    shuffle cards fixed

--- main/startpage.js
+++ main/startpage.js
@@ -21,7 +21,7 @@ function checkOptionsSelected() {
 }
 
 function generateLink() {
-    let theme = colored[1] === "flags" ? "./data/flags.json" : "./data/cards.json";
+    let theme = colored[1] === "flags" ? "./data/flags.json" : (colored[1] === "cards" ? "./data/cards.json" : " ./data/animals.json");
     let difficulty = colored[3] === "easy" ? 4 : (colored[3] === "medium" ? 6 : 9);
     let link = `?theme=${theme}&player=${colored[2]}&level=${colored[3]}&difficulty=${difficulty}`;
     window.location.href = "index.html" + link;
