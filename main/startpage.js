4b17d3a76dc9a3f19f1aae59847f76bff0d11fc7
SafaeKa <safae.kartite@nttdata.com>
2024-04-28 19:59:58 +0200

    Player names are also taken in the generated link

--- main/startpage.js
+++ main/startpage.js
@@ -70,9 +70,17 @@ function generateGameLink() {
         theme = random[n];
     }
     let difficulty = colored[3] === "easy" ? 4 : (colored[3] === "medium" ? 6 : 9);
-    let link = `?theme=${theme}&player=${colored[2]}&level=${colored[3]}&difficulty=${difficulty}`;
+    const player1Name = document.getElementById('player1Name').value.trim();
+    const player2Name = document.getElementById('player2Name').value.trim();
+    let link;
+    if (colored[2] === "two") {
+        link = `?theme=${theme}&player=two&level=${colored[3]}&difficulty=${difficulty}&player1Name=${player1Name}&player2Name=${player2Name}`;
+    } else {
+        link = `?theme=${theme}&player=solo&level=${colored[3]}&difficulty=${difficulty}&player1Name=${player1Name}`;
+    }
     window.location.href = "game.html" + link;
 }
+
 
 
 function togglePlayerInput(playerType) {
