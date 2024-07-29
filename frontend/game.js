df07de0dd3e9ed946d39958ee636eb9dcdcecedc
SafaeKa <safae.kartite@nttdata.com>
2024-07-29 12:59:40 +0200

    corrected save scores logic

--- frontend/game.js
+++ frontend/game.js
@@ -172,10 +172,10 @@ class Game {
     }
 
     async saveScores() {
-        let calculateScorePlayer1 = this.player1.score / this.player1.attempts * 10;
+        let calculateScorePlayer1 = this.player1.attempts === 0 ? 0 : this.player1.score / this.player1.attempts * 10;
         let calculateScorePlayer2;
         if (this.player2) {
-            calculateScorePlayer2 = this.player2.score / this.player2.attempts * 10;
+            calculateScorePlayer2 = this.player2.attempts === 0 ? 0 : this.player2.score / this.player2.attempts * 10;
         }
         const url = 'https://memory-backend-application-a3d1c61211c9.herokuapp.com/player';
 
